9e60f11bc856ebe01774645eb09d965c00e34656
Andrew Johnson <ajj@byu.net>
2017-12-22 14:12:41 -0600

    Play page created

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,38 +1,63 @@
 $(document).ready(start);
 
+let numberOfPlayers;
+
 function start() {
     console.log('JQ Sourced');
 
-    $('#startButton').on('click', createPlayMode);
+    $('#startButton').on('click', newPage);
+}
+
+// Make a new page
+function newPage() {
+
+    createPlayMode();
+    if (numberOfPlayers <= 4 && numberOfPlayers > 0) {
+
+        let newMain = $('<div id="main">');
+
+        for (let i = 0; i < numberOfPlayers; i++) {
+            let newPlayer = $('<div class="player">');
+            newPlayer.append('<input>');
+            newPlayer.append('<button>Guess!</button>');
+            newMain.append(newPlayer);
+        }
+
+
+        // numberOfPlayers;
+
+
+        $('body').empty().append(newMain);
+    }
 }
 
 function createPlayMode() {
     let maxNumber = setDifficulty();
     let numberOfPlayers = setNumberPlayers();
 
-    $.ajax({
-        method: 'POST',
-        url: '/guess',
-        data: {
-            object: 'object'
-        },
-        success: function(response) {
-            console.log('in post');
+    // $.ajax({
+    //     method: 'POST',
+    //     url: '/guess',
+    //     data: {
+    //         object: 'object'
+    //     },
+    //     success: function(response) {
+    //         console.log('in post');
 
-            $.ajax({
-                method: 'GET',
-                url: '/guess',
-                success: function(response) {
-                    console.log('in post\'s get!', response);
-                }
-            });
+    //         $.ajax({
+    //             method: 'GET',
+    //             url: '/guess',
+    //             success: function(response) {
+    //                 console.log('in post\'s get!', response);
+    //             }
+    //         });
 
-        }
-    });
+    //     }
+    // });
 
-    if(numberOfPlayers) {
-        runPlayMode(maxNumber, numberOfPlayers);
-    }
+    // if(numberOfPlayers) {
+    //     runPlayMode(maxNumber, numberOfPlayers);
+    // }
 
 }
 
@@ -56,7 +81,7 @@ function setDifficulty() {
 }
 
 function setNumberPlayers() {
-    let numberOfPlayers = parseInt($('#numberOfPlayers').val());
+    numberOfPlayers = parseInt($('#numberOfPlayers').val());
     if (numberOfPlayers <= 4 && numberOfPlayers > 0) {
         return numberOfPlayers;
     } else {
